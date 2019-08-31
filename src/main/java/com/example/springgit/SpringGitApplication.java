1dceb8892b12cc0bbbfd182d86a16c17d9af5cb1
chenzhiyu <279349250@qq.com>
2019-08-31 09:33:23 +0800

    修改spring-git项目

--- src/main/java/com/example/springgit/SpringGitApplication.java
+++ src/main/java/com/example/springgit/SpringGitApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringGitApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(SpringGitApplication.class, args);
     }
---
+++ src/main/java/com/example/springgit/web/DemoController.java
@@ -0,0 +1,8 @@
+package com.example.springgit.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+}
